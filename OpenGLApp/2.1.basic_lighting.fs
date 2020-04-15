d12d84e1510acf3b3420cae6d3973d36d10c96e2
harmeets43 <harmeets43@gmail.com>
2020-04-15 16:07:28 -0500

    Gouraud Shading method.

--- OpenGLApp/2.1.basic_lighting.fs
+++ OpenGLApp/2.1.basic_lighting.fs
@@ -3,32 +3,24 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec3 FragPos;
-in vec3 Normal;
-in vec3 LightPos;   // extra in variable, since we need the light position in view space we calculate this in the vertex shader
+in vec3 LightingColor; 
 
-uniform vec3 lightColor;
 uniform vec3 objectColor;
 
 void main()
 {
-    // ambient
-    float ambientStrength = 0.1;
-    vec3 ambient = ambientStrength * lightColor;    
-    
-     // diffuse 
-    vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(LightPos - FragPos);
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
-    
-    // specular
-    float specularStrength = 0.5;
-    vec3 viewDir = normalize(-FragPos); // the viewer is always at (0,0,0) in view-space, so viewDir is (0,0,0) - Position => -Position
-    vec3 reflectDir = reflect(-lightDir, norm);  
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = specularStrength * spec * lightColor; 
-    
-    vec3 result = (ambient + diffuse + specular) * objectColor;
-    FragColor = vec4(result, 1.0);
+   FragColor = vec4(LightingColor * objectColor, 1.0);
 }
+
+
+
+//So what do we see?
+//You can see (for yourself or in the provided image) the clear distinction of the two triangles at the front of the 
+//cube. This 'stripe' is visible because of fragment interpolation. From the example image we can see that the top-right 
+//vertex of the cube's front face is lit with specular highlights. Since the top-right vertex of the bottom-right triangle is 
+//lit and the other 2 vertices of the triangle are not, the bright values interpolates to the other 2 vertices. The same 
+//happens for the upper-left triangle. Since the intermediate fragment colors are not directly from the light source 
+//but are the result of interpolation, the lighting is incorrect at the intermediate fragments and the top-left and 
+//bottom-right triangle collide in their brightness resulting in a visible stripe between both triangles.
+
+//This effect will become more apparent when using more complicated shapes.
