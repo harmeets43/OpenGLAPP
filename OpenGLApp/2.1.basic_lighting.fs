cbc5302a875d68ee14519e1bf6ec350dee7e6eba
harmeets43 <harmeets43@gmail.com>
2020-04-15 16:17:58 -0500

    Make the cube change color based on a material color

--- OpenGLApp/2.1.basic_lighting.fs
+++ OpenGLApp/2.1.basic_lighting.fs
@@ -1,26 +1,45 @@
-// Fragment shader:
-// ================
 #version 330 core
 out vec4 FragColor;
 
-in vec3 LightingColor; 
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;    
+    float shininess;
+}; 
 
-uniform vec3 objectColor;
+struct Light {
+    vec3 position;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+in vec3 FragPos;  
+in vec3 Normal;  
+  
+uniform vec3 viewPos;
+uniform Material material;
+uniform Light light;
 
 void main()
 {
-   FragColor = vec4(LightingColor * objectColor, 1.0);
-}
-
-
-
-//So what do we see?
-//You can see (for yourself or in the provided image) the clear distinction of the two triangles at the front of the 
-//cube. This 'stripe' is visible because of fragment interpolation. From the example image we can see that the top-right 
-//vertex of the cube's front face is lit with specular highlights. Since the top-right vertex of the bottom-right triangle is 
-//lit and the other 2 vertices of the triangle are not, the bright values interpolates to the other 2 vertices. The same 
-//happens for the upper-left triangle. Since the intermediate fragment colors are not directly from the light source 
-//but are the result of interpolation, the lighting is incorrect at the intermediate fragments and the top-left and 
-//bottom-right triangle collide in their brightness resulting in a visible stripe between both triangles.
-
-//This effect will become more apparent when using more complicated shapes.
+    // ambient
+    vec3 ambient = light.ambient * material.ambient;
+  	
+    // diffuse 
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(light.position - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = light.diffuse * (diff * material.diffuse);
+    
+    // specular
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);  
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = light.specular * (spec * material.specular);  
+        
+    vec3 result = ambient + diffuse + specular;
+    FragColor = vec4(result, 1.0);
+} 
