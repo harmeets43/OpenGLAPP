41874adef3fb6544d7c24c0ab525cc9669c40faf
harmeets43 <harmeets43@gmail.com>
2020-04-15 18:15:59 -0500

    Adding emmission map on the object.

--- OpenGLApp/2.1.basic_lighting.fs
+++ OpenGLApp/2.1.basic_lighting.fs
@@ -3,9 +3,10 @@ out vec4 FragColor;
 
 struct Material {
     sampler2D diffuse;
-    sampler2D specular;
-    float     shininess;
-};  
+    sampler2D specular;    
+    sampler2D emission;
+    float shininess;
+}; 
 
 struct Light {
     vec3 position;
@@ -26,19 +27,23 @@ uniform Light light;
 void main()
 {
     // ambient
-    vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
+    vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
   	
     // diffuse 
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(light.position - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));  
+    vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;  
     
     // specular
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = light.specular * spec * (vec3(1.0) - vec3(texture(material.specular, TexCoords))); // here we inverse the sampled specular color. Black becomes white and white becomes black.
+    vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;  
+    
+      // emission
+    vec3 emission = texture(material.emission, TexCoords).rgb;
         
-    FragColor = vec4(ambient + diffuse + specular, 1.0);  
-}
+    vec3 result = ambient + diffuse + specular + emission;
+    FragColor = vec4(result, 1.0);
+} 
